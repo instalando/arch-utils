d9fb7c60bb3f954127d3c744cdd91988db0c3ea4
instalando <instalando@protonmail.com>
2022-06-06 16:49:40 -0300

    fix: path of mirrors

--- scripts/install-system-base.sh
+++ scripts/install-system-base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pacman -S reflector --noconfirm
-reflector -c Brazil > /mnt/etc/pacman.d/mirrorlist
+reflector -c Brazil > /etc/pacman.d/mirrorlist
 pacman -Syu linux linux-headers grub efibootmgr wget nano networkmanager --noconfirm
