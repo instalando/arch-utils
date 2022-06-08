9dd38d4c4e13b2e574d61f0ddd1c84686275ce7a
instalando <instalando@protonmail.com>
2022-06-08 15:21:44 -0300

    feat: add uefi mode gnome

---
+++ gnome-install-uefi-with-pt-br-locale copy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+chmod +x ${pwd}scripts/*
+
+./scripts/install-system-base.sh
+
+# grub ~ mbr
+./scripts/grub-uefi.sh
+
+# Update system
+./scripts/updade-system.sh
+
+# update sudoers
+arch-chroot /mnt "%wheel ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers
+
+# Install Gnome Base
+./scripts/install-gnome.sh
+
+# set defaullt user
+./scripts/create-default-user.sh
+
+# Set locale pt-br
+./scripts/locale-pt-br.sh
