265994750309c0f199eb06a9afd8c132a1729844
instalando <instalando@protonmail.com>
2022-06-08 15:41:52 -0300

    feat: update recovery-pkg.sh

--- utils/recovery-pkg.sh
+++ utils/recovery-pkg.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
 pacman -S --needed - < ../pkgs-list/pkglist.txt
-
-pacman -S git --noconfirm
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-
-yay -S < ./pkgs-list/aur-pkglist.txt
