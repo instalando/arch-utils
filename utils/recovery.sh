fcd5a2826e6e2c1d58a44239cd1abd3c01aba610
instalando <instalando@protonmail.com>
2022-06-08 15:51:13 -0300

    fix: recovery

--- utils/recovery.sh
+++ utils/recovery.sh
@@ -4,7 +4,9 @@ chmod +x ${pwd}*
 
 ./save-pkgs.sh
 
+pacman -Sc --noconfirm
 pacman -D --asdeps $(pacman -Qqe)
 pacman -D --asexplicit base linux linux-firmware
+pacman -Qtdq | pacman -Rns -
 
 ./recovery-pkg.sh
