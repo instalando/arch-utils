ddf1cf83538a25e00cb0139e9004eae5fdeaf1de
instalando <instalando@protonmail.com>
2022-06-11 18:33:13 -0300

    feat: add xfce4 installs

---
+++ xfce4_install_uefi_pt_BR.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+chmod +x ${pwd}scripts/*
+
+./scripts/install-system-base.sh
+
+# grub ~ uefi
+./scripts/grub-uefi.sh
+
+# Update system
+./scripts/updade-system.sh
+
+# update sudoers
+arch-chroot /mnt "%wheel ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers
+
+# Install GDM
+./scripts/install-gdm.sh
+
+# Install xfce4 Base
+./scripts/install-xfce4.sh
+
+# set defaullt user
+./scripts/create-default-user.sh
+
+# Set locale pt-br
+./scripts/locale-pt-br.sh
