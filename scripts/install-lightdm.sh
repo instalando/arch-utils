6ca62207fd55770f04f017faf24738bfa2619001
instalando <instalando@protonmail.com>
2022-06-11 19:46:29 -0300

    chore: update lightdm

--- scripts/install-lightdm.sh
+++ scripts/install-lightdm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pacman -S lightdm lightdm-gtk-greeter xorg xorg-server --noconfirm
-echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf
+pacman -S mesa lightdm-gtk-greeter xorg --noconfirm
 systemctl enable lightdm
